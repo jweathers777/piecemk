71922f9c781b55103b2e951f302736c9a6f2ee96
John Weathers <johnweathers@gmail.com>
2011-01-26 10:48:00 -0500

    Add dai/tai data, koma calculation changes, scans of king and pawn

--- lib/piece_maker/koma.rb
+++ lib/piece_maker/koma.rb
@@ -1,40 +1,80 @@
 module PieceMaker
   class Koma
     attr_reader :width, :height
+    #
+    #               O = (0,0)
+    #  O________________________________   
+    #  |               inner width      |    ^
+    #  |                    |           |    |
+    #  |                    |           |    |
+    #  |               P2   |           |    |
+    #  |               |    V           |    |
+    #  |       P1------|-------P3       |    |
+    #  |               |       |        |    |
+    #  |               |       |        |    |
+    #  |               |       | inner  |    |   square height
+    #  |       height  |       | height |    |
+    #  |               |       |        |    |
+    #  |               |       |        |    |
+    #  |               |       |        |    |
+    #  |               |       |        |    |
+    #  |   P0          |       |   P4   |    |
+    #  |   <---------width---------->   |    |
+    #  |                                |    |
+    #  |________________________________|    v
+    #                                  
+    #  <---------square-width----------->                                  
+    #                                  
+    #
+    #  major_side = |P0-P1|
+    #  minor_side = |P1-P2|
+    #  theta = Angle P1-P0-P4
+    #  phi = Angle P2-P1-P3
 
-    SCALE = 3.3114754098360657
+    SCALE = 33.918181818181814
     DIMENSIONS = [
-       { 
-         :width => 2.0, :height => 2.1784967336952059, 
-         :major_side_length => 2.2, :major_side_angle => 1.4308665149210393, 
-         :minor_side_length => 0.7, :minor_side_angle => 0.13992981187385722 
+       {
+         :width => 22.5, :height => 27.0, 
+         :major_side_length => 22.5, :theta => 1.413716694115407, 
+         :minor_side_length => 9.087145770278516, :phi => 2.082703807444008
        },
        {
-         :width => 2.2, :height => 2.3646668685905188, 
-         :major_side_length => 2.4, :major_side_angle => 1.3989917626074015, 
-         :minor_side_length => 0.7, :minor_side_angle => 0.17978546233291781
+         :width => 23.5, :height => 28.0, 
+         :major_side_length => 23.5, :theta => 1.413716694115407, 
+         :minor_side_length => 9.387422993470103, :phi => 2.082703807444008
        },
        {
-         :width => 2.3, :height => 2.2716747920588776, 
-         :major_side_length => 2.3, :major_side_angle => 1.4136933736338848, 
-         :minor_side_length => 0.8, :minor_side_angle => 0.15710295316101175
+         :width => 25.5, :height => 29.0, 
+         :major_side_length => 25.5, :theta => 1.413716694115407, 
+         :minor_side_length => 9.555099861716837, :phi => 2.082703807444008
        },
        {
-         :width => 2.5, :height => 2.5246081695959628, 
-         :major_side_length => 2.55, :major_side_angle => 1.4295578749398743, 
-         :minor_side_length => 0.9, :minor_side_angle => 0.14123845185502226
+         :width => 26.7, :height => 30.0, 
+         :major_side_length => 26.7, :theta => 1.413716694115407, 
+         :minor_side_length => 9.864847316814341, :phi => 2.082703807444008
        },
        {
-         :width => 2.6, :height => 2.6825511579847716, 
-         :major_side_length => 2.7, :major_side_angle => 1.4570465415216347, 
-         :minor_side_length => 1.0, :minor_side_angle => 0.11374978527326185
+         :width => 27.7, :height => 31.0, 
+         :major_side_length => 27.7, :theta => 1.413716694115407, 
+         :minor_side_length => 10.189501615937441, :phi => 2.082703807444008
        },
        {
-         :width => 2.8, :height => 2.7696976484401667, 
-         :major_side_length => 2.8, :major_side_angle => 1.4235424970034918, 
-         :minor_side_length => 1.0, :minor_side_angle => 0.14725382979140478
+         :width => 28.7, :height => 32.0, 
+         :major_side_length => 28.7, :theta => 1.413716694115407, 
+         :minor_side_length => 10.515372151562836, :phi => 2.082703807444008
        }
     ]
+
+    def self.calculate_minor_side_length(width, height, theta)
+      edge_width = width*Math.cos(theta)
+      inner_height = width*Math.sin(theta)
+
+      inner_width = width - 2*edge_width
+      top_height = height - inner_height
+      top_half_width = inner_width/2
+
+      Math.sqrt(top_height**2 + top_half_width**2)
+    end
 
     def initialize(size, square_width, square_height)
       @index = size - 1
@@ -56,38 +96,12 @@ module PieceMaker
     end
 
     def vertices
-      #
-      #               O = (0,0)
-      #  O________________________________   
-      #  |                                |    ^
-      #  |                                |    |   major_side = |P0-P1|
-      #  |                                |    |   minor_side = |P1-P2|
-      #  |               P2               |    |
-      #  |               |                |    |   major_side_angle = Angle P1-P0-P4
-      #  |       P1      |       P3       |    |   minor_side_angle = Angle P2-P1-P3
-      #  |               |                |    |
-      #  |               |                |    |
-      #  |               |                |    |   square height
-      #  |       height  |                |    |
-      #  |               |                |    |
-      #  |               |                |    |
-      #  |               |                |    |
-      #  |               |                |    |
-      #  |   P0          |           P4   |    |
-      #  |   <---------width---------->   |    |
-      #  |                                |    |
-      #  |________________________________|    v
-      #                                  
-      #  <---------square-width----------->                                  
-      #                                  
-      #
-
       @vertices ||= 
         begin
           major_side_length = (DIMENSIONS[@index][:major_side_length] / SCALE) * @square_width
           minor_side_length = (DIMENSIONS[@index][:minor_side_length] / SCALE) * @square_width
-          major_side_angle = DIMENSIONS[@index][:major_side_angle]
-          minor_side_angle = DIMENSIONS[@index][:minor_side_angle]
+          theta = DIMENSIONS[@index][:theta]
+          phi = DIMENSIONS[@index][:phi]
 
           vertices = [0]*8
           vertices[0] = ((@square_width - @width)/2).to_i
@@ -96,14 +110,14 @@ module PieceMaker
           vertices[8] = ((@square_width + @width)/2).to_i
           vertices[9] = vertices[1]
     
-          vertices[2] = (vertices[0] + major_side_length * Math.cos(major_side_angle)).to_i
-          vertices[3] = (vertices[1] - major_side_length * Math.sin(major_side_angle)).to_i
+          vertices[2] = (vertices[0] + major_side_length * Math.cos(theta)).to_i
+          vertices[3] = (vertices[1] - major_side_length * Math.sin(theta)).to_i
           
-          vertices[6] = (vertices[8] - major_side_length * Math.cos(major_side_angle)).to_i
+          vertices[6] = (vertices[8] - major_side_length * Math.cos(theta)).to_i
           vertices[7] = vertices[3]
 
           vertices[4] = (0.5*vertices[0] + 0.5*vertices[8]).to_i
-          vertices[5] = (vertices[3] - minor_side_length*Math.sin(minor_side_angle)).to_i
+          vertices[5] = (vertices[3] - minor_side_length*Math.sin(phi)).to_i
 
           vertices
         end
